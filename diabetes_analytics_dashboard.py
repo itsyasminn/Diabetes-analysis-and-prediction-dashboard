a63765de7d2e89f5b73d92da379ffc1f5e604b14
itsyasminn <yazzyshazzy@gmail.com>
2026-03-13 17:12:34 +0300

    Fix multipage structure

--- diabetes_analytics_dashboard.py
+++ diabetes_analytics_dashboard.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
+    page_title="Diabetes Analytics Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.set_page_config(
     page_title="Diabetes Prediction Dashboard",
     layout="wide"
 )
